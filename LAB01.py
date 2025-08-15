3649b30d17d4c5ad5dd9807d82452dfe74d9d1e1
Gabriella Fernanda <gabriella-nanda@hotmail.com>
2025-08-15 01:24:52 -0300

    LAB01S01: criacao das queries

--- LAB01.py
+++ LAB01.py
@@ -20,3 +20,64 @@ HEADERS = {
 
 # URL da API GraphQL do GitHub
 GRAPHQL_URL = "https://api.github.com/graphql"
+
+# Query 1: Informações básicas dos repositórios (leve)
+BASIC_REPOS_QUERY = """
+query($first: Int!, $after: String) {
+  search(query: "stars:>1 sort:stars-desc", type: REPOSITORY, first: $first, after: $after) {
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+    nodes {
+      ... on Repository {
+        id
+        name
+        owner {
+          login
+        }
+        url
+        stargazerCount
+        createdAt
+        updatedAt
+        primaryLanguage {
+          name
+        }
+        forkCount
+        watchers {
+          totalCount
+        }
+        description
+      }
+    }
+  }
+  rateLimit {
+    remaining
+    resetAt
+  }
+}
+"""
+
+# Query 2: Métricas pesadas para repositórios específicos
+HEAVY_METRICS_QUERY = """
+query($owner: String!, $name: String!) {
+  repository(owner: $owner, name: $name) {
+    pullRequests(states: [MERGED]) {
+      totalCount
+    }
+    releases {
+      totalCount
+    }
+    issues {
+      totalCount
+    }
+    closedIssues: issues(states: [CLOSED]) {
+      totalCount
+    }
+  }
+  rateLimit {
+    remaining
+    resetAt
+  }
+}
+"""
